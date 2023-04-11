b31ea457f70df362cba692ccdeb147ebcd3abebc
FeBotero <frpbotero@gmail.com>
2023-04-10 20:26:02 -0400

    Create menu and insert log application

--- app/src/main/java/com/motoacademy/lablogin/MainActivity.java
+++ app/src/main/java/com/motoacademy/lablogin/MainActivity.java
@@ -3,12 +3,40 @@ package com.motoacademy.lablogin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btSing = findViewById(R.id.btSing);
+
+        btSing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.v("Hello","Hello");
+            }
+        });
+
     }
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+
+
+    }
+
+
+
 }
